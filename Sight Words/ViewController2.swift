57c31bc1e3a10dbcceb3c9939d9e3934e40cfb4b
mdrouillard <markdrouillard@gmail.com>
2017-02-28 10:47:54 -0500

    Added actions to buttons, correct and wrong indicator to see if they're working.

--- Sight Words/ViewController2.swift
+++ Sight Words/ViewController2.swift
@@ -12,6 +12,9 @@ import UIKit
 var seconds = 60
 var timer = Timer()
 
+// for debugging
+var correctWord = "a"
+
 class ViewController2: UIViewController {
    
     // text fields
@@ -31,9 +34,9 @@ class ViewController2: UIViewController {
        // start the timer
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController2.counter), userInfo: nil, repeats: true)
      
-        answerButton1.setTitle("a", for: .normal)
+        answerButton1.setTitle("away", for: .normal)
         answerButton2.setTitle("and", for: .normal)
-        answerButton3.setTitle("away", for: .normal)
+        answerButton3.setTitle("a", for: .normal)
         answerButton4.setTitle("big", for: .normal)
 
         
@@ -100,5 +103,42 @@ class ViewController2: UIViewController {
 
    }
     
+    @IBAction func buttonOneAction(_ sender: UIButton) {
+        // if correct, get new word, get new answers, remove correct word from array, add to timer, add to score
+        if answerButton1.titleLabel?.text == correctWord {
+            print("Correct!")
+            
+        } else {
+            print("Wrong!")
+        }
+    }
+    
+    @IBAction func buttonTwoAction(_ sender: Any) {
+        if answerButton2.titleLabel?.text == correctWord {
+            print("Correct!")
+        } else {
+            print("Wrong!")
+        }
+
+        
+    }
+    
+    @IBAction func buttonThreeAction(_ sender: Any) {
+        if answerButton3.titleLabel?.text == correctWord {
+            print("Correct!")
+        } else {
+            print("Wrong!")
+        }
+
+    }
+    
+    @IBAction func buttonFourAction(_ sender: UIButton) {
+        if answerButton4.titleLabel?.text == correctWord {
+            print("Correct!")
+        } else {
+            print("Wrong!")
+        }
+
+    }
     
 }
